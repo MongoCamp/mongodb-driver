941d4d6082e5e478102da682b6311495e95e197c
sfxcode <tom@sfxcode.com>
2018-05-08 10:50:43 +0200

    add relation handling

---
+++ src/main/scala/com/sfxcode/nosql/mongo/relation/DefaultRelationCache.scala
@@ -0,0 +1,24 @@
+package com.sfxcode.nosql.mongo.relation
+
+import scala.collection.mutable
+
+class DefaultRelationCache extends RelationCaching {
+  val cachedRelations = new mutable.HashMap[String, AnyRef]()
+
+  def addCachedValue(key: String, value: AnyRef): Unit = {
+    cachedRelations.+=(key -> value)
+  }
+
+  def getCachedValue[B <: AnyRef](key: String): B = {
+    cachedRelations(key).asInstanceOf[B]
+  }
+
+  def hasCachedValue(key: String): Boolean = {
+    cachedRelations.contains(key)
+  }
+
+  def removeCachedValue(key: String): Unit = {
+    cachedRelations.remove(key)
+  }
+
+}
---
+++ src/main/scala/com/sfxcode/nosql/mongo/relation/Relationship.scala
@@ -0,0 +1,35 @@
+package com.sfxcode.nosql.mongo.relation
+
+import com.sfxcode.nosql.mongo.relation.RelationCache._
+import com.sfxcode.nosql.mongo.{ MongoDAO, _ }
+
+abstract class Relationship {
+  val id: String = hashCode().toString
+
+  def removeFromCache(value: Any): Unit = {
+    val key = "%s_%s".format(id, value)
+    removeCachedValue(key)
+  }
+}
+
+case class OneToOneRelationship[A](dao: MongoDAO[A], daoKey: String, useCache: Boolean = true) extends Relationship {
+
+  def relatedRecord(value: Any): Option[A] = {
+    val key = "%s_%s".format(id, value)
+    if (!useCache || !hasCachedValue(key))
+      addCachedValue(key, dao.find(daoKey, value).result())
+    getCachedValue[Option[A]](key)
+  }
+
+}
+
+case class OneToManyRelationship[A](dao: MongoDAO[A], daoKey: String, useCache: Boolean = true) extends Relationship {
+
+  def relatedRecords(value: Any): List[A] = {
+    val key = "%s_%s".format(id, value)
+    if (!useCache || !hasCachedValue(key))
+      addCachedValue(key, dao.find(daoKey, value).resultList())
+    getCachedValue[List[A]](key)
+  }
+
+}
