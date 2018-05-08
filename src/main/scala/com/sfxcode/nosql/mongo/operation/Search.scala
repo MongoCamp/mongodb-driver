c5086a452409a89d6076763870ce290da952c25e
Tom <tom@sfxcode.com>
2018-05-08 10:54:21 +0200

    Move distinct frm Base to Search

--- src/main/scala/com/sfxcode/nosql/mongo/operation/Search.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/operation/Search.scala
@@ -1,9 +1,12 @@
 package com.sfxcode.nosql.mongo.operation
 
+import com.sfxcode.nosql.mongo._
+import com.sfxcode.nosql.mongo.bson.BsonConverter._
+import org.bson.BsonValue
 import org.mongodb.scala.bson.ObjectId
 import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.Filters._
-import org.mongodb.scala.{ AggregateObservable, Document, FindObservable, MongoCollection }
+import org.mongodb.scala.{AggregateObservable, DistinctObservable, Document, FindObservable, MongoCollection}
 
 import scala.reflect.ClassTag
 
@@ -21,6 +24,14 @@ abstract class Search[A]()(implicit ct: ClassTag[A]) extends Crud[A] {
   }
 
   def find(name: String, value: Any): FindObservable[A] = find(equal(name, value))
+
+  def distinct[S <: Any](fieldName: String, filter: Bson = Document()): DistinctObservable[BsonValue] = {
+    coll.distinct[BsonValue](fieldName, filter)
+  }
+
+  def distinctResult[S <: Any](fieldName: String, filter: Bson = Document()): Seq[S] = {
+    distinct(fieldName, filter).results().map(v => fromBson(v).asInstanceOf[S])
+  }
 
   def findAggregated(aggregator: Seq[Bson]): AggregateObservable[A] = coll.aggregate(aggregator)
 
