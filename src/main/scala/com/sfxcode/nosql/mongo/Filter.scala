71403742a5dbcd9d0d0262736e9b2f57654dc036
sfxcode <tom@sfxcode.com>
2018-05-08 10:53:55 +0200

    Add date in range filter

--- src/main/scala/com/sfxcode/nosql/mongo/Filter.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/Filter.scala
@@ -1,10 +1,12 @@
 package com.sfxcode.nosql.mongo
 
+import java.util.Date
+
 import com.typesafe.scalalogging.LazyLogging
 import org.bson.conversions.Bson
 import org.mongodb.scala.bson.BsonDocument
 import org.mongodb.scala.bson.collection.immutable.Document
-import org.mongodb.scala.model.Filters.{ equal, in, not }
+import org.mongodb.scala.model.Filters._
 
 object Filter extends LazyLogging {
   val DefaultBson: Bson = BsonDocument(Document())
@@ -25,4 +27,16 @@ object Filter extends LazyLogging {
   def nullFilter(fieldName: String): Bson = equal(fieldName, null)
 
   def notNullFilter(fieldName: String): Bson = not(nullFilter(fieldName))
+
+  def dateInRangeFilter(dateFieldKey: String, dateFrom: Date = null, dateUntil: Date = null): Bson = {
+    if (dateFrom != null && dateUntil != null)
+      and(gte(dateFieldKey, dateFrom), lte(dateFieldKey, dateUntil))
+    else if (dateUntil != null)
+      lte(dateFieldKey, dateUntil)
+    else if (dateFrom != null)
+      gte(dateFieldKey, dateFrom)
+    else
+      Map()
+  }
+
 }
