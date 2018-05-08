8dd0bc6ebe0b58b7e40bcfde5bb2461cf7d9ed05
Tom <tom@sfxcode.com>
2018-05-08 10:50:43 +0200

    add relation handling

---
+++ src/main/scala/com/sfxcode/nosql/mongo/relation/Relations.scala
@@ -0,0 +1,13 @@
+package com.sfxcode.nosql.mongo.relation
+
+trait Relations {
+
+  def relatedRecordForOneToOne[A <: AnyRef](relationship: OneToOneRelationship[A], value: Any): Option[A] = {
+    relationship.relatedRecord(value)
+  }
+
+  def relatedRecordForOneToMany[A <: AnyRef](relationship: OneToManyRelationship[A], value: Any): List[A] = {
+    relationship.relatedRecords(value)
+  }
+
+}
