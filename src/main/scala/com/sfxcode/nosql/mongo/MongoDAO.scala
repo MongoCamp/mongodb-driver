c84e6a1c51de23d974b2fd1c3e1fa4adfee85672
Tom <tom@sfxcode.com>
2018-07-08 09:46:46 +0200

    GridFS Support

--- src/main/scala/com/sfxcode/nosql/mongo/MongoDAO.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/MongoDAO.scala
@@ -1,6 +1,6 @@
 package com.sfxcode.nosql.mongo
 
-import com.sfxcode.nosql.mongo.operation.Search
+import com.sfxcode.nosql.mongo.operation.Crud
 import org.mongodb.scala.{ Document, MongoCollection, MongoDatabase }
 
 import scala.reflect.ClassTag
@@ -8,7 +8,7 @@ import scala.reflect.ClassTag
 /**
  * Created by tom on 20.01.17.
  */
-abstract class MongoDAO[A](database: MongoDatabase, collectionName: String)(implicit ct: ClassTag[A]) extends Search[A] {
+abstract class MongoDAO[A](database: MongoDatabase, collectionName: String)(implicit ct: ClassTag[A]) extends Crud[A] {
 
   val collection: MongoCollection[A] = database.getCollection[A](collectionName)
 
