4976b4fccabac08e65b79b90bb70ea61fbaf2cb8
Tom <tom@sfxcode.com>
2018-07-08 09:48:06 +0200

    GridFS implicits ObjectID implicits DatabaseProvider with default registry

--- src/main/scala/com/sfxcode/nosql/mongo/database/DatabaseProvider.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/database/DatabaseProvider.scala
@@ -3,16 +3,22 @@ package com.sfxcode.nosql.mongo.database
 import com.sfxcode.nosql.mongo.bson.codecs.CustomCodecProvider
 import org.bson.codecs.configuration.CodecRegistries.fromProviders
 import org.bson.codecs.configuration.CodecRegistry
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala.{ MongoClient, MongoDatabase }
 
-case class DatabaseProvider(databaseName: String, registry: CodecRegistry, client: MongoClient = MongoClient()) {
+case class DatabaseProvider(databaseName: String, registry: CodecRegistry = DEFAULT_CODEC_REGISTRY, client: MongoClient = MongoClient()) {
 
   import org.bson.codecs.configuration.CodecRegistries.fromRegistries
   import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 
   private val CustomRegistry = fromProviders(CustomCodecProvider())
 
-  private val codecRegistry: CodecRegistry = fromRegistries(registry, CustomRegistry, DEFAULT_CODEC_REGISTRY)
+  val codecRegistry: CodecRegistry = {
+    if (registry == DEFAULT_CODEC_REGISTRY)
+      fromRegistries(registry, CustomRegistry)
+    else
+      fromRegistries(registry, CustomRegistry, DEFAULT_CODEC_REGISTRY)
+  }
 
   val database: MongoDatabase = client.getDatabase(databaseName).withCodecRegistry(codecRegistry)
 
