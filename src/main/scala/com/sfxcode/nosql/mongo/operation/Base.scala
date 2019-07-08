c43d07b5a760e3f25c8cf08d6b951c8f62b2f2d3
tom <tom@sfxcode.com>
2019-07-08 21:38:37 +0200

    add some index functions

--- src/main/scala/com/sfxcode/nosql/mongo/operation/Base.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/operation/Base.scala
@@ -2,9 +2,10 @@ package com.sfxcode.nosql.mongo.operation
 
 import com.typesafe.scalalogging.LazyLogging
 import org.mongodb.scala.bson.conversions.Bson
+import org.mongodb.scala.model.Indexes.{hashed, text}
 import org.mongodb.scala.model.Sorts._
-import org.mongodb.scala.model.{ CountOptions, IndexOptions }
-import org.mongodb.scala.{ Completed, Document, MongoCollection, Observable, SingleObservable }
+import org.mongodb.scala.model.{CountOptions, DropIndexOptions, IndexOptions}
+import org.mongodb.scala.{Completed, Document, ListIndexesObservable, MongoCollection, Observable, SingleObservable}
 
 import scala.reflect.ClassTag
 
@@ -17,19 +18,30 @@ abstract class Base[A]()(implicit ct: ClassTag[A]) extends LazyLogging {
 
   def drop(): Observable[Completed] = coll.drop()
 
-  def createIndexForField(field: String, sortAscending: Boolean = true): SingleObservable[String] =
+  def createIndexForField(field: String, sortAscending: Boolean = true,  options: IndexOptions = IndexOptions()): SingleObservable[String] =
     if (sortAscending) {
-      createIndex(ascending(field))
+      createIndex(ascending(field), options)
     } else {
-      createIndex(descending(field))
+      createIndex(descending(field), options)
     }
+
+  def createUniqueIndexForField(collectionName: String,
+                        fieldName: String,
+                        ascending: Boolean = true): Unit =
+    createIndexForField(fieldName, ascending, IndexOptions().unique(true))
 
   def createIndex(key: Bson, options: IndexOptions = IndexOptions()): SingleObservable[String] =
     coll.createIndex(key, options)
 
-  def dropIndexForName(name: String): SingleObservable[Completed] =
-    coll.dropIndex(name)
+  def dropIndexForName(name: String, options: DropIndexOptions = new DropIndexOptions()): SingleObservable[Completed] =
+    coll.dropIndex(name, options)
 
-  def dropIndex(keys: Bson): SingleObservable[Completed] = coll.dropIndex(keys)
+  def dropIndex(keys: Bson, options: DropIndexOptions = new DropIndexOptions()): SingleObservable[Completed] = coll.dropIndex(keys, options)
+
+  def createHashedIndex(fieldName: String,
+                        options: IndexOptions = IndexOptions()): SingleObservable[String] =
+    coll.createIndex(hashed(fieldName), options)
+
+  def listIndexes: ListIndexesObservable[Map[String, Any]] = coll.listIndexes[Map[String, Any]]()
 
 }
