9bd5f631c6caec61f43b2abc2f8772be33694054
tom <tom@sfxcode.com>
2020-04-18 14:25:04 +0200

    refactor Index Handling

--- src/main/scala/com/sfxcode/nosql/mongo/operation/Base.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/operation/Base.scala
@@ -1,12 +1,13 @@
 package com.sfxcode.nosql.mongo.operation
 
+import com.sfxcode.nosql.mongo.database.MongoIndex
 import com.typesafe.scalalogging.LazyLogging
 import org.mongodb.scala.bson.conversions.Bson
-import org.mongodb.scala.model.Indexes.hashed
 import org.mongodb.scala.model.Sorts._
-import org.mongodb.scala.model.{ CountOptions, DropIndexOptions, IndexOptions }
+import org.mongodb.scala.model.{ CountOptions, DropIndexOptions, IndexOptions, Indexes }
 import org.mongodb.scala.{ Document, ListIndexesObservable, MongoCollection, Observable, SingleObservable }
 
+import scala.concurrent.duration.Duration
 import scala.reflect.ClassTag
 
 abstract class Base[A]()(implicit ct: ClassTag[A]) extends LazyLogging {
@@ -18,18 +19,39 @@ abstract class Base[A]()(implicit ct: ClassTag[A]) extends LazyLogging {
 
   def drop(): Observable[Void] = coll.drop()
 
-  def createIndexForField(field: String, sortAscending: Boolean = true, options: IndexOptions = IndexOptions()): SingleObservable[String] =
+  def createIndexForField(fieldName: String,
+                          sortAscending: Boolean = true,
+                          options: IndexOptions = IndexOptions()): SingleObservable[String] =
     if (sortAscending) {
-      createIndex(ascending(field), options)
+      createIndex(ascending(fieldName), options)
     } else {
-      createIndex(descending(field), options)
+      createIndex(descending(fieldName), options)
     }
 
-  def createUniqueIndexForField(
-    collectionName: String,
-    fieldName: String,
-    ascending: Boolean = true): Unit =
-    createIndexForField(fieldName, ascending, IndexOptions().unique(true))
+  def createIndexForFieldWithName(fieldName: String,
+                          sortAscending: Boolean = true,
+                          name: String): SingleObservable[String] = {
+    createIndexForField(fieldName, sortAscending, MongoIndex.indexOptionsWithName(Some(name)))
+  }
+
+  def createUniqueIndexForField(fieldName: String,
+                                sortAscending: Boolean = true,
+                                name: Option[String] = None): SingleObservable[String] =
+    createIndexForField(fieldName, sortAscending, MongoIndex.indexOptionsWithName(name).unique(true))
+
+  def createHashedIndexForField(fieldName: String, options: IndexOptions = IndexOptions()): SingleObservable[String] =
+    createIndex(Indexes.hashed(fieldName), IndexOptions())
+
+  def createTextIndexForField(fieldName: String, options: IndexOptions = IndexOptions()): SingleObservable[String] =
+    createIndex(Indexes.text(fieldName), IndexOptions())
+
+  def createExpiringIndexForField(fieldName: String,
+                                  duration: Duration,
+                                  sortAscending: Boolean = true,
+                                  name: Option[String] = None): SingleObservable[String] =
+    createIndexForField(fieldName,
+                        sortAscending,
+                        MongoIndex.indexOptionsWithName(name).expireAfter(duration._1, duration._2))
 
   def createIndex(key: Bson, options: IndexOptions = IndexOptions()): SingleObservable[String] =
     coll.createIndex(key, options)
@@ -37,13 +59,13 @@ abstract class Base[A]()(implicit ct: ClassTag[A]) extends LazyLogging {
   def dropIndexForName(name: String, options: DropIndexOptions = new DropIndexOptions()): SingleObservable[Void] =
     coll.dropIndex(name, options)
 
-  def dropIndex(keys: Bson, options: DropIndexOptions = new DropIndexOptions()): SingleObservable[Void] = coll.dropIndex(keys, options)
-
-  def createHashedIndex(
-    fieldName: String,
-    options: IndexOptions = IndexOptions()): SingleObservable[String] =
-    coll.createIndex(hashed(fieldName), options)
+  def dropIndex(keys: Bson, options: DropIndexOptions = new DropIndexOptions()): SingleObservable[Void] =
+    coll.dropIndex(keys, options)
 
   def listIndexes: ListIndexesObservable[Map[String, Any]] = coll.listIndexes[Map[String, Any]]()
+
+  def indexList(): List[MongoIndex] = MongoIndex.convertIndexDocumentsToMongoIndexList(listIndexes)
+
+  def hasIndexForField(fieldName: String) = MongoIndex.hasIndexForFieldWithName(listIndexes, fieldName)
 
 }
---
+++ src/test/scala/com/sfxcode/nosql/mongo/operation/IndexSpec.scala
@@ -0,0 +1,96 @@
+package com.sfxcode.nosql.mongo.operation
+
+import com.sfxcode.nosql.mongo.TestDatabase._
+import com.sfxcode.nosql.mongo._
+import com.sfxcode.nosql.mongo.database.MongoIndex
+import org.specs2.mutable.Specification
+import org.specs2.specification.BeforeAll
+
+class IndexSpec extends Specification with BeforeAll {
+
+  sequential
+
+  "Base Index Operations" should {
+
+    "return an index list" in {
+
+      val list = PersonDAO.indexList
+      list must haveSize(1)
+
+      val mongoIndex: MongoIndex = list.head
+      mongoIndex.name must be equalTo  "_id_"
+      mongoIndex.key must be equalTo  "_id"
+      mongoIndex.ascending must be equalTo  1
+      mongoIndex.namespace must be equalTo  "simple_mongo_test.person"
+      mongoIndex.version must be equalTo 2
+      mongoIndex.keys must haveSize(1)
+      mongoIndex.keys.head._1 must be equalTo "_id"
+      mongoIndex.keys.head._2 must be equalTo 1
+
+    }
+
+    "create / drop indexes for key" in {
+
+      var createIndexResult: String = PersonDAO.createIndexForField("name")
+
+      createIndexResult must be equalTo "name_1"
+
+      PersonDAO.indexList must haveSize(2)
+
+      val dropIndexResult: Void = PersonDAO.dropIndexForName(createIndexResult)
+
+      PersonDAO.indexList must haveSize(1)
+    }
+
+    "evaluate has index" in {
+
+      PersonDAO.hasIndexForField("_id") must beTrue
+
+      PersonDAO.hasIndexForField("unknown") must beFalse
+    }
+
+    "create descending index for key" in {
+
+      var createIndexResult: String = PersonDAO.createIndexForFieldWithName("name", sortAscending = false, "myIndex")
+
+      createIndexResult must be equalTo "myIndex"
+
+      PersonDAO.indexList must haveSize(2)
+
+      PersonDAO.dropIndexForName(createIndexResult).headResult()
+
+      PersonDAO.indexList must haveSize(1)
+    }
+
+    "create unique index for key" in {
+
+      var createIndexResult: String = PersonDAO.createUniqueIndexForField("id", sortAscending = false, Some("myUniqueIndex"))
+
+      createIndexResult must be equalTo "myUniqueIndex"
+
+      PersonDAO.indexList must haveSize(2)
+
+      PersonDAO.dropIndexForName(createIndexResult).headResult()
+
+      PersonDAO.indexList must haveSize(1)
+    }
+
+    "create hashed index for key" in {
+
+      var createIndexResult: String = PersonDAO.createHashedIndexForField("email")
+
+      createIndexResult must be equalTo "email_hashed"
+
+      PersonDAO.indexList must haveSize(2)
+
+      PersonDAO.dropIndexForName(createIndexResult).headResult()
+
+      PersonDAO.indexList must haveSize(1)
+    }
+
+
+  }
+
+  override def beforeAll: Unit = BookDAO.drop().headResult()
+
+}
