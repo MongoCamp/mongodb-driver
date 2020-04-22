42c13e00a2f7bf113ae94981747b7959ef072f46
tom <tom@sfxcode.com>
2020-04-22 19:23:08 +0200

    add ChangeObserver

--- src/main/scala/com/sfxcode/nosql/mongo/database/DatabaseProvider.scala
+++ src/main/scala/com/sfxcode/nosql/mongo/database/DatabaseProvider.scala
@@ -40,6 +40,11 @@ class DatabaseProvider(config: MongoConfig, registry: CodecRegistry) extends Ser
     cachedDatabaseMap(databaseName)
   }
 
+  def addChangeObserver(observer: ChangeObserver[Document]): ChangeObserver[Document] = {
+    database().watch().subscribe(observer)
+    observer
+  }
+
   def collection(collectionName: String): MongoCollection[Document] =
     dao(collectionName).collection
 
@@ -60,7 +65,7 @@ class DatabaseProvider(config: MongoConfig, registry: CodecRegistry) extends Ser
     database(name).listCollectionNames()
 
   case class DocumentDao(provider: DatabaseProvider, collectionName: String)
-    extends MongoDAO[Document](this, collectionName)
+      extends MongoDAO[Document](this, collectionName)
 
 }
 
@@ -75,9 +80,8 @@ object DatabaseProvider {
   def apply(config: MongoConfig, registry: CodecRegistry = codecRegistry): DatabaseProvider =
     new DatabaseProvider(config, fromRegistries(registry, CustomRegistry, DEFAULT_CODEC_REGISTRY))
 
-  def fromPath(
-    configPath: String = MongoConfig.DefaultConfigPathPrefix,
-    registry: CodecRegistry = codecRegistry): DatabaseProvider =
+  def fromPath(configPath: String = MongoConfig.DefaultConfigPathPrefix,
+               registry: CodecRegistry = codecRegistry): DatabaseProvider =
     apply(MongoConfig.fromPath(configPath), fromRegistries(registry, CustomRegistry, DEFAULT_CODEC_REGISTRY))
 
 }
--- src/test/scala/com/sfxcode/nosql/mongo/TestDatabase.scala
+++ src/test/scala/com/sfxcode/nosql/mongo/TestDatabase.scala
@@ -1,17 +1,32 @@
 package com.sfxcode.nosql.mongo
 
-import com.sfxcode.nosql.mongo.database.DatabaseProvider
+import com.mongodb.client.model.changestream.OperationType
+import com.sfxcode.nosql.mongo.database.{ ChangeObserver, DatabaseProvider }
 import com.sfxcode.nosql.mongo.model._
+import com.typesafe.scalalogging.LazyLogging
 import org.bson.codecs.configuration.CodecRegistries._
+import org.mongodb.scala.Document
 import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.model.changestream.ChangeStreamDocument
 
-object TestDatabase {
+object TestDatabase extends LazyLogging {
 
   private val registry           = fromProviders(classOf[Person], classOf[Friend], classOf[CodecTest])
   private val universityRegistry = fromProviders(classOf[Student], classOf[Score], classOf[Grade])
 
   val provider =
     DatabaseProvider.fromPath(configPath = "unit.test.mongo", registry = fromRegistries(registry, universityRegistry))
+
+  provider.addChangeObserver(ChangeObserver(consumeDatabaseChanges))
+
+  def consumeDatabaseChanges(changeStreamDocument: ChangeStreamDocument[Document]): Unit =
+    if (changeStreamDocument.getOperationType != OperationType.INSERT) {
+      logger.info(
+        "changed %s:%s with ID: %s".format(changeStreamDocument.getNamespace,
+                                           changeStreamDocument.getOperationType,
+                                           changeStreamDocument.getDocumentKey)
+      )
+    }
 
   object PersonDAO extends MongoDAO[Person](provider, "people")
 
