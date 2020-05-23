760a2d84e1cefb07df0cf175d2460f8c904b4f76
tom <tom@sfxcode.com>
2020-05-23 18:26:30 +0200

    Update Docs

--- docs/src/main/scala/UniversityApp.scala
+++ docs/src/main/scala/UniversityApp.scala
@@ -1,7 +1,10 @@
 import com.sfxcode.nosql.mongo.test.UniversityDatabase._
 import com.sfxcode.nosql.mongo._
+import com.sfxcode.nosql.mongo.server.LocalServer
 
 object UniversityApp extends App {
+
+  val LocalTestServer = LocalServer()
 
   val collections = provider.collections().resultList()
 
