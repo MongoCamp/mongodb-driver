a6be199326dc3ada9f0f38c17ddf109a003d43fe
tom <tom@sfxcode.com>
2020-10-08 10:04:46 +0200

    sbt 1.4.0

--- src/test/scala/com/sfxcode/nosql/mongo/operation/CrudSpec.scala
+++ src/test/scala/com/sfxcode/nosql/mongo/operation/CrudSpec.scala
@@ -1,12 +1,16 @@
 package com.sfxcode.nosql.mongo.operation
 
-import com.sfxcode.nosql.mongo.test.TestDatabase._
 import com.sfxcode.nosql.mongo._
 import com.sfxcode.nosql.mongo.dao.PersonSpecification
+import com.sfxcode.nosql.mongo.database.DatabaseProvider
 import com.sfxcode.nosql.mongo.model.CodecTest
+import com.sfxcode.nosql.mongo.test.TestDatabase._
 import com.typesafe.scalalogging.LazyLogging
+import org.bson.conversions.Bson
 import org.mongodb.scala.bson.ObjectId
-import org.mongodb.scala.model.changestream.ChangeStreamDocument
+import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.result.DeleteResult
+import org.mongodb.scala.model.Updates._
 
 class CrudSpec extends PersonSpecification with LazyLogging {
 
@@ -15,32 +19,52 @@ class CrudSpec extends PersonSpecification with LazyLogging {
   override def beforeAll(): Unit = {
     super.beforeAll()
     CodecDao.drop().result()
-    // CodecDao.addChangeObserver(ChangeObserver(consumeCodecChanges))
-    val result = CodecDao.insertOne(CodecTest()).result()
-
-    val list: List[CodecTest] = CodecDao.find().resultList()
-    list.size mustEqual 1
-
-    list.head._id must haveClass[ObjectId]
-
-    def consumeCodecChanges(changeStreamDocument: ChangeStreamDocument[CodecTest]): Unit =
-      logger.info(
-        "codec changed %s:%s with ID: %s".format(
-          changeStreamDocument.getNamespace,
-          changeStreamDocument.getOperationType,
-          changeStreamDocument.getDocumentKey
-        )
-      )
 
   }
 
   "Crud Operations" should {
 
-    "create Documents in" in {
+    "create Document in" in {
+      val result = CodecDao.insertOne(CodecTest()).result()
 
-      CodecDao.count().result() must be equalTo 1
-      val findOneResult: Option[CodecTest] = CodecDao.find("id", 1).resultOption()
-      findOneResult must beSome[CodecTest]
+      val list: List[CodecTest] = CodecDao.find().resultList()
+      list.size mustEqual 1
+
+    }
+
+    "update Document in" in {
+      var list: List[CodecTest] = CodecDao.find().resultList()
+      var codec                 = list.head
+      codec.id mustEqual 1
+      CodecDao.updateOne(Map("id" -> 1), set("id", 2)).result()
+      list = CodecDao.find().resultList()
+      codec = list.head
+      codec.id mustEqual 2
+    }
+
+    "replace Document in" in {
+      var list: List[CodecTest] = CodecDao.find().resultList()
+      var codec                 = list.head
+      codec.id mustEqual 2
+      CodecDao.replaceOne(codec.copy(id = 1)).result()
+      list = CodecDao.find().resultList()
+      codec = list.head
+      codec.id mustEqual 1
+    }
+
+    "delete Document in" in {
+      val hexString = CodecDao.find().result()._id.toHexString
+      hexString must not beEmpty
+
+      val result: DeleteResult =
+        CodecDao.deleteOne(equal(DatabaseProvider.ObjectIdKey, new ObjectId(hexString))).result()
+
+      result.wasAcknowledged() must beTrue
+      result.getDeletedCount must beEqualTo(1)
+
+      val list: List[CodecTest] = CodecDao.find().resultList()
+
+      list.size mustEqual 0
     }
 
   }
