27f80452ec29dcfe8013b180ccd8ecfa6b300ff8
QuadStingray <git@quadstingray.dev>
2023-11-02 22:39:36 +0100

    test: Added Joda Duration Converter to plugin sample

--- src/test/scala/dev/mongocamp/driver/mongodb/bson/JodaConverterPlugin.scala
+++ src/test/scala/dev/mongocamp/driver/mongodb/bson/JodaConverterPlugin.scala
@@ -1,15 +1,18 @@
 package dev.mongocamp.driver.mongodb.bson
 
 import org.bson.BsonValue
-import org.joda.time.DateTime
-import org.mongodb.scala.bson.{BsonDateTime, BsonNull}
+import org.joda.time.{DateTime, Duration}
+import org.mongodb.scala.bson.{BsonDateTime, BsonNull, BsonString}
 
 class JodaConverterPlugin extends AbstractConverterPlugin {
-  override def customClassList: List[Class[_]] = List(classOf[DateTime])
+  override def customClassList: List[Class[_]] = List(classOf[DateTime], classOf[Duration])
 
   override def toBson(value: Any): BsonValue =
     value match {
-      case dt: DateTime => BsonDateTime(dt.toDate)
+      case dt: DateTime =>
+        BsonDateTime(dt.toDate)
+      case dt: org.joda.time.Duration =>
+        BsonString(s"${dt.getMillis}ms")
       case _ =>
         BsonNull()
 
