8c50fe6a7b2d68149138b205430068596d422e34
QuadStingray <git@quadstingray.dev>
2024-04-26 23:15:51 +0200

    feat: implemented jdbc driver methods

--- src/main/scala/dev/mongocamp/driver/mongodb/jdbc/MongoJdbcDriver.scala
+++ src/main/scala/dev/mongocamp/driver/mongodb/jdbc/MongoJdbcDriver.scala
@@ -1,46 +1,45 @@
 package dev.mongocamp.driver.mongodb.jdbc
 
-import com.mongodb.MongoCredential.createCredential
 import com.vdurmont.semver4j.Semver
 import dev.mongocamp.driver.mongodb.BuildInfo
-import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
-import org.mongodb.scala.{ConnectionString, MongoClient, MongoClientSettings, MongoCredential}
+import dev.mongocamp.driver.mongodb.database.{ DatabaseProvider, MongoConfig }
+import org.mongodb.scala.{ ConnectionString, ServerAddress }
 
-import java.sql.{Connection, DriverPropertyInfo}
+import java.sql.{ Connection, DriverPropertyInfo }
 import java.util.Properties
 import java.util.logging.Logger
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 class MongoJdbcDriver extends java.sql.Driver {
 
   private lazy val semVer = new Semver(BuildInfo.version)
 
-
-  /**
-   * Connect to the database using a URL like :
-   * jdbc:mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-   * The URL excepting the jdbc: prefix is passed as it is to the MongoDb native Java driver.
-   */
+  /** Connect to the database using a URL like : jdbc:mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]] The
+    * URL excepting the jdbc: prefix is passed as it is to the MongoDb native Java driver.
+    */
   override def connect(url: String, info: Properties): Connection = {
     if (url == null || !acceptsURL(url)) {
       return null
     }
 
     val connectionUrl = url.replaceFirst("^jdbc:", "")
-    val username = info.getProperty("user")
-    val password = info.getProperty("password")
+    val username      = Option(info.getProperty("user")).filter(_.trim.nonEmpty)
+    val password      = Option(info.getProperty("password")).filter(_.trim.nonEmpty)
 
-    val builder = MongoClientSettings
-      .builder()
-      .applyConnectionString(new ConnectionString(connectionUrl))
-      .codecRegistry(DEFAULT_CODEC_REGISTRY)
-
-    if (!username.equalsIgnoreCase("") && !password.equalsIgnoreCase("")) {
-      val credential: MongoCredential = createCredential(username, "admin", password.toCharArray)
-      builder.credential(credential).build()
-    }
-
-    val client: MongoClient = MongoClient(builder.build())
-    new MongoJdbcConnection(client)
+    val string = new ConnectionString(connectionUrl)
+    val provider = DatabaseProvider(
+      MongoConfig(
+        string.getDatabase,
+        MongoConfig.DefaultHost,
+        MongoConfig.DefaultPort,
+        string.getApplicationName,
+        username,
+        password,
+        string.getDatabase,
+        serverAddressList = string.getHosts.asScala.toList.map(h => new ServerAddress(h))
+      )
+    )
+    new MongoJdbcConnection(provider)
   }
 
   override def acceptsURL(url: String): Boolean = {
