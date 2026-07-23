cbbe5e18a1b8170cdad467480a0aa5535d7b08a5
QuadStingray <github@quadstingray.dev>
2026-07-23 11:55:12 +0200

    feat: enhance FileUtils to support multiple input types (URL, URI, Path, and String) and resolve JAR resource paths

--- src/main/scala/dev/mongocamp/driver/mongodb/utils/FileUtils.scala
+++ src/main/scala/dev/mongocamp/driver/mongodb/utils/FileUtils.scala
@@ -2,14 +2,19 @@ package dev.mongocamp.driver.mongodb.utils
 import better.files.File
 import java.net.URI
 import java.net.URL
+import java.nio.file.FileSystem
+import java.nio.file.FileSystemNotFoundException
+import java.nio.file.FileSystems
+import java.nio.file.Path
 
 object FileUtils {
 
-  def getFileByUrl(url: URL): File = {
+  def getFile(url: URL): File = {
     if (url.getProtocol == "file") {
       File(url)
     }
     else {
+      resolveResourcePath(url.toURI)
       val tempFile = File.newTemporaryFile("mongodb-driver-import", ".json")
       tempFile.deleteOnExit()
       val inputStream = url.openStream()
@@ -20,6 +25,25 @@ object FileUtils {
     }
   }
 
-  def getFileByUrl(uri: URI): File = getFileByUrl(uri.toURL)
+  def getFile(uri: URI): File = getFile(uri.toURL)
 
+  def getFile(string: String): File = getFile(new URI(string))
+
+  def getFile(path: Path): File = getFile(path.toString)
+
+  def resolveResourcePath(uri: URI): (Path, Option[FileSystem]) = {
+    if (uri.getScheme == "jar") {
+      val fs =
+        try
+          FileSystems.getFileSystem(uri)
+        catch {
+          case _: FileSystemNotFoundException =>
+            FileSystems.newFileSystem(uri, java.util.Collections.emptyMap[String, Any]())
+        }
+      (fs.getPath(uri.getSchemeSpecificPart.substring(uri.getSchemeSpecificPart.indexOf('!') + 1)), Some(fs))
+    }
+    else {
+      (Path.of(uri), None)
+    }
+  }
 }
